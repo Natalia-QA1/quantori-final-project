46beb63cff6e20e4e5d437a0ce1f538a5eb12c93
Natalia Ananeva <ananeva.natalia92@gmail.com>
2024-07-01 12:20:35 +0200

    Added project structure tree.

---
+++ molecules_similarities_project/config/config.py
@@ -0,0 +1,13 @@
+# AWS Postgres Credentials
+PG_HOST = "host"
+PG_PORT = '5432'
+PG_USER = "user"
+PG_PASSWORD = "password"
+PG_DATABASE = "database"
+
+
+# AWS S3 credentials
+AWS_ACCESS_KEY = "a_key"
+AWS_SECRET_KEY = "s_key"
+REGION_NAME = "region"
+AWS_SESSION_TOKEN = "token"
