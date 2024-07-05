9849e14db451c9db6de063cec3f0f49a324fbbb4
Natalia Ananeva <ananeva.natalia92@gmail.com>
2024-07-05 14:51:44 +0200

    Deleted procedure insert_data_dm_top10_fct() since we will load into fact datamart table after retriving data fron s3.

--- molecules_similarities_project/sql_scripts/dwh_datamart_tables_data_load_pipepline_script.sql
+++ molecules_similarities_project/sql_scripts/dwh_datamart_tables_data_load_pipepline_script.sql
@@ -1,66 +1,3 @@
-create or replace procedure nananeva.insert_data_dm_top10_fct()
-language plpgsql
-as $$
-begin
-	with ranked_similarities as (
-    select
-        target_chembl_id,
-        source_chembl_id,
-        similarity_score,
-        row_number() over (partition by target_chembl_id order by similarity_score desc) as row_num,
-        dense_rank() over (partition by target_chembl_id order by similarity_score desc) as dense_rnk
-    from nananeva.st_fct_target_molecules_data_03_06_2024_similarities
-    where source_chembl_id != target_chembl_id
-	),
-	top_10_similarities as (
-	    select
-	        target_chembl_id,
-	        source_chembl_id,
-	        similarity_score,
-	        row_num,
-	        dense_rnk,
-	        case when row_num = 10 then similarity_score else null
-	        end as tenth_similarity_score
-	    from ranked_similarities
-	),
-	tenth_similarity as (
-	    select distinct
-	        target_chembl_id,
-	        similarity_score as tenth_similarity_score
-	    from top_10_similarities
-	    where row_num = 10
-	),
-	top_10_with_duplicates as (
-	    select
-	        rs.target_chembl_id,
-	        rs.source_chembl_id,
-	        rs.similarity_score,
-	        case when rs.row_num > 10 and rs.similarity_score = ts.tenth_similarity_score then true else false
-	        end as has_duplicates_of_last_largest_score
-	    from ranked_similarities rs
-	    left join tenth_similarity ts
-	    on rs.target_chembl_id = ts.target_chembl_id
-	       and rs.similarity_score = ts.tenth_similarity_score
-	    where rs.row_num <= 10 or (rs.row_num > 10 and rs.similarity_score = ts.tenth_similarity_score)
-	)
-	insert into nananeva.dm_top10_fct_molecules_similarities_data_03_06_2024 (
-	target_molecule_reference,
-	source_molecule_reference,
-	tanimoto_similarity_score,
-	has_duplicates_of_last_largest_score
-	)
-	select
-	    target_chembl_id,
-	    source_chembl_id,
-	    similarity_score,
-	    has_duplicates_of_last_largest_score
-	from top_10_with_duplicates
-	where not exists(select 1 from nananeva.dm_top10_dim_molecules_data_03_06_2024
-	;
-
-end
-$$
-
 
 create or replace procedure nananeva.insert_data_dm_top10_dim()
 language plpgsql
@@ -68,12 +5,12 @@ as $$
 begin
 	with data_mart_chembl_ids as(
 	select distinct(target_molecule_reference) as chembl_id
-	from nananeva.dm_top10_fct_molecules_similarities_data_03_06_2024
+	from nananeva.dm_top10_fct_molecules_similarities
 	union all
 	select distinct(source_molecule_reference) as chembl_id
-	from nananeva.dm_top10_fct_molecules_similarities_data_03_06_2024
+	from nananeva.dm_top10_fct_molecules_similarities
 	)
-	insert into nananeva.dm_top10_dim_molecules_data_03_06_2024 (
+	insert into nananeva.dm_top10_dim_molecules (
 		chembl_id ,
 	    molecule_type ,
 	    mw_freebase ,
@@ -99,21 +36,9 @@ begin
 	join nananeva.st_dim_compound_properties mp
 	on md.chembl_id = mp.chembl_id
 	where md.chembl_id in (select chembl_id from data_mart_chembl_ids)
-	      and not exists(select 1 from nananeva.dm_top10_dim_molecules_data_03_06_2024;
+	      and not exists(select 1 from nananeva.dm_top10_dim_molecules;
 
 end
 $$
 
-
-create or replace procedure nananeva.insert_data_dm_top10_pipeline_main()
-language plpgsql
-as $$
-begin
-call nananeva.insert_data_dm_top10_fct();
 call nananeva.insert_data_dm_top10_dim();
-
-end
-$$
-
-
-call nananeva.insert_data_dm_top10_pipeline_main();
